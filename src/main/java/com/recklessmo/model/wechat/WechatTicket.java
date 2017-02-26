30a4551965476bf43cfe70f95b26304367aa4c68
胡鹏飞 <hupengfei@ruoshui.me>
2017-02-26 19:04:05 +0800

    微信绑定

---
+++ src/main/java/com/recklessmo/model/wechat/WechatTicket.java
@@ -0,0 +1,56 @@
+package com.recklessmo.model.wechat;
+
+import java.util.Date;
+
+/**
+ * Created by hpf on 1/12/17.
+ */
+public class WechatTicket {
+
+    private long id;
+    private long orgId;
+    private String sid;
+    private String ticket;
+    private Date created;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(long orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
