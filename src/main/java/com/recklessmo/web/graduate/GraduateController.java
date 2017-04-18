b4a886d0f9dd1d9846f2b42a9249e3d57a3753b5
胡鹏飞 <hupengfei@ruoshui.me>
2017-04-18 18:21:29 +0800

    修复前端界面

--- src/main/java/com/recklessmo/web/graduate/GraduateController.java
+++ src/main/java/com/recklessmo/web/graduate/GraduateController.java
@@ -3,9 +3,13 @@ package com.recklessmo.web.graduate;
 import com.recklessmo.model.exam.Exam;
 import com.recklessmo.model.graduate.Graduate;
 import com.recklessmo.model.security.DefaultUserDetails;
+import com.recklessmo.model.setting.Grade;
+import com.recklessmo.model.student.StudentAddInfo;
+import com.recklessmo.model.student.StudentAllInfo;
 import com.recklessmo.response.JsonResponse;
 import com.recklessmo.service.exam.ExamService;
 import com.recklessmo.service.graduate.GraduateService;
+import com.recklessmo.service.student.StudentService;
 import com.recklessmo.util.ContextUtils;
 import com.recklessmo.web.webmodel.page.ExamListPage;
 import com.recklessmo.web.webmodel.page.Page;
@@ -26,6 +30,9 @@ public class GraduateController {
     @Resource
     private GraduateService graduateService;
 
+    @Resource
+    private StudentService studentService;
+
     @ResponseBody
     @RequestMapping(value = "/list", method = RequestMethod.POST)
     public JsonResponse listGraduate(@RequestBody Page page){
@@ -40,6 +47,11 @@ public class GraduateController {
     public JsonResponse addGraduate(@RequestBody Graduate graduate){
         DefaultUserDetails userDetails = ContextUtils.getLoginUserDetail();
         graduate.setOrgId(userDetails.getOrgId());
+        graduate.setOpId(userDetails.getId());
+        graduate.setOpName(userDetails.getName());
+        graduate.setCreated(new Date());
+        StudentAllInfo studentAllInfo = studentService.getStudentInfoBySid(userDetails.getOrgId(), graduate.getSid());
+        graduate.setName(studentAllInfo.getName());
         graduateService.addGraduate(graduate);
         return new JsonResponse(200, null, null);
     }
--- src/main/webapp/js/custom/student/list/graduate/student-graduate-list.controller.js
+++ src/main/webapp/js/custom/student/list/graduate/student-graduate-list.controller.js
@@ -3,37 +3,84 @@
     angular
         .module('custom')
         .controller('StudentGraduateListController', StudentGraduateListController);
-    StudentGraduateListController.$inject = ['$scope','GraduateService', 'SweetAlert', 'NgTableParams', 'blockUI', 'Notify'];
+    StudentGraduateListController.$inject = ['$scope', 'ngDialog', 'GraduateService', 'SweetAlert', 'NgTableParams', 'blockUI', 'Notify'];
 
-    function StudentGraduateListController($scope, GraduateService, SweetAlert, NgTableParams, blockUI, Notify) {
+    function StudentGraduateListController($scope, ngDialog, GraduateService, SweetAlert, NgTableParams, blockUI, Notify) {
 
         $scope.graduateDataList = [];
 
-        $scope.$on('chooseSid', function(event, data){
+        $scope.$on('chooseSid', function (event, data) {
             $scope.sid = data;
             $scope.activate();
         });
 
-        $scope.activate = function() {
+        $scope.activate = function () {
             blockUI.start();
-            GraduateService.getGraduateList({sid : $scope.sid}).success(function(data){
+            GraduateService.getGraduateList({sid: $scope.sid}).success(function (data) {
                 blockUI.stop();
                 if (data.status == 200) {
                     $scope.graduateDataList = data.data;
                     $scope.showTables();
-                }else{
+                } else {
                     SweetAlert.error("服务器内部错误, 请联系客服!");
                 }
-            }).error(function(){
+            }).error(function () {
                 blockUI.stop();
                 SweetAlert.error("网络问题,请稍后重试!");
             });
         }
 
-        $scope.showTables = function(){
-            $scope.graduateTableParams = new NgTableParams({},{
+        $scope.showTables = function () {
+            $scope.graduateTableParams = new NgTableParams({}, {
                 counts: [],
                 dataset: $scope.graduateDataList
+            });
+        }
+
+        //添加 & 编辑
+        //type为1 代表编辑, type为2 添加
+        $scope.edit = function (item, type) {
+            item['sid'] = $scope.sid;
+            var dialog = ngDialog.open({
+                template: 'app/views/custom/student/list/graduate/student-graduate-add.html',
+                controller: 'StudentGraduateAddController',
+                className: 'ngdialog-theme-default custom-width-800',
+                data: {data: item, type: type}
+            });
+            dialog.closePromise.then(function (data) {
+                if (data.value != 'reload') {
+                    return;
+                }
+                $scope.activate();
+            });
+        };
+
+        //删除
+        $scope.delete = function(id){
+            SweetAlert.swal({
+                title: '确认删除?',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#DD6B55',
+                confirmButtonText: '是',
+                cancelButtonText: '否',
+                closeOnConfirm: true,
+                closeOnCancel: true
+            }, function(isConfirm){
+                if (isConfirm) {
+                    //这里可以进行调试,查看$scope,因为table会创建一个子scope
+                    //然后子scope里面就不能用this了,因为this就指向了子scope,
+                    //实际上在table的每一行里面的点击是调用了父scope的delete方法
+                    blockUI.start();
+                    GraduateService.deleteGraduate(id).success(function () {
+                        Notify.alert("删除成功!", {status:"success", timeout: 3000});
+                        $scope.activate();
+                        blockUI.stop();
+                    }).error(function(){
+                        blockUI.stop();
+                        SweetAlert.error("网络问题, 请稍后重试!");
+                    });
+                }
             });
         }
 
